5411b09ccd9bac7111a1c6bf6f5bb472000957e9
kawsarmahmud12 <kawsarmahmud204@gmail.com>
2023-12-16 18:37:51 +0600

    uploaded via file

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/data.json');
+const chef = require('./data/AllData.json')
+
+app.use(cors());
+
+app.get('/', (req,res) => {
+    res.send('food recipe server is running...')
+})
+
+app.get('/categories', (req,res) => {
+    res.send(categories);
+})
+
+app.get('/chef', (req,res) => {
+    res.send(chef);
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const recipe = chef?.find(c => c.id == id);
+    res.send(recipe);
+})
+
+app.listen(port, () => {
+    console.log(`food recipe API is running on port: ${port}`);
+})
