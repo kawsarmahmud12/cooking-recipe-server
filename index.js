0a8d9edb566c9c4ea48f5ebf840c8d3b6d844016
kawsarmahmud12 <kawsarmahmud204@gmail.com>
2023-12-20 22:12:41 +0600

    uploaded

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 7000;
+const port = process.env.PORT || 7000;
 
 const categories = require('./data/data.json');
 const chef = require('./data/AllData.json')
 
+app.use(cors())
+
 const corsConfig = {
-    origin: 'https://delicate-cajeta-754e6b.netlify.app/',
+    origin: ['https://delicate-cajeta-754e6b.netlify.app/','http://localhost:5173/'],
     credentials: true,
     methods: [
         "GET",
@@ -18,8 +20,8 @@ const corsConfig = {
         "OPTIONS"
     ]
 }
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+// app.use(cors(corsConfig))
+// app.options("", cors(corsConfig))
 
 app.get('/', (req,res) => {
     res.send('food recipe server is running...')
